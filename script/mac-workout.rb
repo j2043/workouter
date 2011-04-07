8956ceb62b704a65f408c4c53e0d4190b2e72e22
Joel Wood <joel@casttv.com>
2011-04-06 20:22:21 -0700

    Remove extranious stuff from script/mac-workout

--- script/mac-workout.rb
+++ script/mac-workout.rb
@@ -2,26 +2,9 @@
 
 require 'yaml'
 require 'activesupport'
+require './lib/mac.rb'
 
-workout = YAML::load_file("config/workouts/basic.yml")
-intervals = 10
-
-def do_segment(activity, interval=30)
-  puts activity
-  sleep interval - 10
-  puts "10 seconds"
-  sleep 8 
-end
-
-do_segment("get ready", 10)
-intervals.times do |count|
-  puts "#{ intervals - count} left"
-  do_segment(workout["slow"].random_element)
-  do_segment(workout["fast"].random_element)
-  do_segment(workout["slow"].random_element)
-  do_segment(workout["fast"].random_element)
-  do_segment("rest")
-end
-
+i =  Workouter.new("basic")
+i.run
 
 
