89f74d4067cb3e6396c4facac55670c260581c04
Joel Wood <jwood@falster.local>
2015-12-06 08:23:27 -0800

    Add bundler bits and usage.

--- script/mac-workout.rb
+++ script/mac-workout.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby 
 
-require 'rubygems'
+require 'bundler/setup'
 require 'yaml'
 require 'thread'
 require 'active_support'
 require './lib/mac.rb'
 workout = ARGV[0]
 segments = ARGV[1].to_i
+
+if workout.nil?
+   puts "You must specify a workout\ntry: #{$0} hard"
+   exit 1
+end
 
 segments = 10 if segments == 0
 
